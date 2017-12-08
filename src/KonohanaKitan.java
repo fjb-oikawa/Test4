3fbc30925c944acc535b0ad54a138eaf0e575b3e
WIN_NT\x16g005 <x16g005@chiba-fjb.ne.jp>
2017-12-08 10:07:57 +0900

    test

--- src/KonohanaKitan.java
+++ src/KonohanaKitan.java
@@ -4,9 +4,7 @@ public class KonohanaKitan {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("此花亭へようこそ！");
-
 		System.out.println("新人仲居の柚と申します！！！");
-
 		System.out.println("ようこそ！お客様！！");
 
 
